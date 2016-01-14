bfc5fad890dba2334b29a2e59c58c11e2b09c90a
Luis Bianchin <labianchin@users.noreply.github.com>
2016-01-13 22:05:45 -0200

    Removes onbuild image

--- build.sh
+++ build.sh
@@ -4,11 +4,4 @@ function build_vanilla() {
     docker build -t mattgruter/artifactory:latest .
 }
 
-function build_onbuild() {
-    cp --no-target-directory --recursive urlrewrite/ onbuild/urlrewrite
-    cat Dockerfile onbuild/trigger > onbuild/Dockerfile
-    docker build -t mattgruter/artifactory:latest-onbuild onbuild
-}
-
 build_vanilla
-build_onbuild
